33e30b56b9b6fc647896d56f008bd63ddc6a579b
Phokham Nonava <pn@nonava.com>
2012-11-14 09:13:48 +0100

    Added license and build files.

---
+++ src/main/java/mdid/Mdid.java
@@ -0,0 +1,26 @@
+/*
+** Copyright 2012 Phokham Nonava
+**
+** This file is part of Message Digest Integrity Database.
+**
+** Message Digest Integrity Database is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** Message Digest Integrity Database is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public License
+** along with Message Digest Integrity Database.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package mdid;
+
+public class Mdid {
+
+	public static void main(String[] args) {
+	}
+	
+}
